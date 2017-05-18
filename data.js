cc9a2ea640b7a0d7e465de8ad686857fee583500
MarenWoodruff <wren403@gmail.com>
2017-05-18 07:43:12 -0400

    updated todos app

--- data.js
+++ data.js
@@ -2,9 +2,11 @@ module.exports = {
   seededTodos: [
     {
       description: "walk Diesel",
+      location: "Old Fourth Ward",
       urgent: true
     }, {
       description: "get coffee",
+      location: "Ponce City Market",
       urgent: true
     }
   ]
